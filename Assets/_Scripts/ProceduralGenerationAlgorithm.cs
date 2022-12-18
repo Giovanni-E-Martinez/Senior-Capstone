6dd95a1a8f56200ad476c0b38d635f2c8346b1d7
Gio <shogunspade@gmail.com>
2022-12-17 23:54:33 -0700

    Added comments to all scripts.

--- Assets/_Scripts/ProceduralGenerationAlgorithm.cs
+++ Assets/_Scripts/ProceduralGenerationAlgorithm.cs
@@ -2,15 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// The class containing the script proper for generating the map.
+/// </summary>
 public static class ProceduralGenerationAlgorithm
 {
+    /// <summary>
+    /// The method responsible for the RandomWalk algorithm.
+    /// </summary>
+    /// <param name="startPos">The starting position of the RandomWalk algorithm.</param>
+    /// <param name="walkLength">The number of units a single RandomWalk iteration will travel.</param>
+    /// <returns></returns>
     public static HashSet<Vector2> RandomWalk(Vector2 startPos, int walkLength)
     {
+        // HashSet used to sstore the RandomWalk path information
         HashSet<Vector2> path = new HashSet<Vector2>();
 
         path.Add(startPos);
         var previousPos = startPos;
 
+        // Randomly change direction each step
         for (int i = 0; i < walkLength; i++)
         {
             var pos = previousPos + Direction2D.getRandomDirection();
@@ -20,12 +31,19 @@ public static class ProceduralGenerationAlgorithm
 
         /*path.Add(new Vector2(1, 1));*/
 
+        // Return the HashMap containing the path information.
         return path;
     }
 }
 
+/// <summary>
+/// Static class used to store the different directions as Vector2 data.
+/// </summary>
 public static class Direction2D
 {
+    /// <summary>
+    /// The four directions that the RandomWalk algorithm can travel.
+    /// </summary>
     public static List<Vector2> cardinalDirections = new List<Vector2>
     {
         new Vector2(0, .5f), // up
@@ -34,6 +52,10 @@ public static class Direction2D
         new Vector2(-.5f, 0) // left
     };
 
+    /// <summary>
+    /// Method used to retrieve a random direction for the cardianlDirections list.
+    /// </summary>
+    /// <returns>Returns the random direction corresponding to the random index number generated.</returns>
     public static Vector2 getRandomDirection()
     {
         return cardinalDirections[Random.Range(0, cardinalDirections.Count)];
--- Assets/_Scripts/RandomWalkGenerator.cs
+++ Assets/_Scripts/RandomWalkGenerator.cs
@@ -6,19 +6,40 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 using Random = UnityEngine.Random;
 
+/// <summary>
+/// Class respondible for implementing and running the RandomWalk algorithm of the ProceduralGeneration scrip and the TileMapVisualizer.
+/// </summary>
 public class RandomWalkGenerator : MonoBehaviour
 {
+    /// <summary>
+    /// Starting position of the generator.
+    /// </summary>
     [SerializeField]
     protected Vector2 startPos = Vector2Int.zero;
+    /// <summary>
+    /// Iterations that the generator will run.
+    /// </summary>
     [SerializeField]
     protected int iterations = 10;
+    /// <summary>
+    /// Number of units that the generator will travel.
+    /// </summary>
     [SerializeField]
     int walkLength = 10;
+    /// <summary>
+    /// Determines wether the generator will start at random points (true) or at the origin (false).
+    /// </summary>
     [SerializeField]
     public bool startRandom = true;
+    /// <summary>
+    /// Instance of the TileMapVisualizer class.
+    /// </summary>
     [SerializeField]
     private TileMapVisualizer tileMapVisualizer;
 
+    /// <summary>
+    /// Runs the RandomWalk algorithm and applies the assets to the tile-map.
+    /// </summary>
     public void RunGeneration()
     {
         HashSet<Vector2> floorPositions = RunRandomWalk();
@@ -26,6 +47,10 @@ public class RandomWalkGenerator : MonoBehaviour
         tileMapVisualizer.PaintFloorTiles(floorPositions);
     }
 
+    /// <summary>
+    /// Method that uses the RandomWalk aglorithm and creates a path to generate the map.
+    /// </summary>
+    /// <returns>Returns the generated path.</returns>
     protected HashSet<Vector2> RunRandomWalk()
     {
         var currentPosition = startPos;
